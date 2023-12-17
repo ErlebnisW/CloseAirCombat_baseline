2ef36b611d23a50235c82f74cbc334ecc0fb75bc
ErlebnisW <787368748w@gmail.com>
2023-12-17 23:39:44 +0800

    comment out wandb

--- scripts/train_share_selfplay.sh
+++ scripts/train_share_selfplay.sh
@@ -6,7 +6,7 @@ exp="v1"
 seed=0
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
-CUDA_VISIBLE_DEVICES=0 python train/train_jsbsim.py \
+CUDA_VISIBLE_DEVICES=1 python train/train_jsbsim.py \
     --env-name ${env} --algorithm-name ${algo} --scenario-name ${scenario} --experiment-name ${exp} \
     --seed ${seed} --n-training-threads 1 --n-rollout-threads 32 --cuda --log-interval 1 --save-interval 1 \
     --num-mini-batch 5 --buffer-size 3000 --num-env-steps 1e8 \
@@ -14,4 +14,5 @@ CUDA_VISIBLE_DEVICES=0 python train/train_jsbsim.py \
     --hidden-size "128 128" --act-hidden-size "128 128" --recurrent-hidden-size 128 --recurrent-hidden-layers 1 --data-chunk-length 8 \
     --use-selfplay --selfplay-algorithm "fsp" --n-choose-opponents 1 \
     --use-eval --n-eval-rollout-threads 1 --eval-interval 1 --eval-episodes 1 \
-    --user-name "jyh" --use-wandb --wandb-name "thu_jsbsim" \
+    --user-name "jyh" \
+    # --use-wandb --wandb-name "thu_jsbsim" \
